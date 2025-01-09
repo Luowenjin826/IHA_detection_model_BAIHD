9da700f07b907ff4366e4a5b3e17afffb5d4bb83
Luo Wenjin <128019485+Luowenjin826@users.noreply.github.com>
2025-01-10 03:34:25 +0800

    Add files via upload

---
+++ web_app/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello"
+)
+
+st.write("# Welcome! ")
+
+st.image("background.jpg", caption="IHA detection app")
+
+st.markdown(
+    """
+    Welcome to use IHA detection app
+"""
+)
